fc3ace51e01d77af93c7fd3c357a8fe26e035d39
Isaac Brito <ibritomorales@gmail.com>
2020-06-30 16:24:17 +1000

    working well

--- yscripts/R_scripts/VoCCPrioritizr_00d_CostLayerDepthSpecies.R
+++ yscripts/R_scripts/VoCCPrioritizr_00d_CostLayerDepthSpecies.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(readr)
 library(stringr)
 
+cost <- readRDS("Cost_Layers/Cost_Raster_Sum.rds")
 species <- read_rds("Cost_Layers/Cost_RasterStack_bySpecies.rds")
 ns_species <- names(species)
 length(ns_species)
@@ -12,8 +13,25 @@ length(ns_species2)
 
 
 depth <- tibble(Species = ns_species2)
+depth2 <- list()
+
+
+
 for (s in 1:length(depth$Species)){
-  eco <- rfishbase::species(ns_species2[s], fields = c("DepthRangeShallow", "DepthRangeDeep"))
-  depth$MinDepth[s] <- eco$depthRangeShallow
-  depth$MaxDepth[s] <- eco$depthRangeDeep
+  depth2[[s]] <- rfishbase::species(ns_species2[s], fields = c("DepthRangeShallow", "DepthRangeDeep"))
+  # depth$MinDepth[s] <- eco$depthRangeShallow
+  # depth$MaxDepth[s] <- eco$depthRangeDeep
 }
+
+names(depth2) <- ns_species2
+depth3 <- do.call(rbind, depth2)
+depth3 <- depth3 %>% 
+  mutate(Species = ns_species2) %>% 
+  select(Species, DepthRangeShallow, DepthRangeDeep)
+
+plot(species$Acanthistius.brasilianus)
+
+cost_all <- stackApply(species, indices = nlayers(species), fun = sum)
+plot(cost_all)
+
+writeRaster(cost_all, "Cost_Layers/Cost_Raster_Sum.tif")
