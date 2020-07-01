5b64558d70d7237bf533f7cd1f9df2dd568e6bfb
Isaac Brito <ibritomorales@gmail.com>
2020-07-01 11:52:00 +1000

    minor updates

--- yscripts/R_scripts/VoCCPrioritizr_00d_CostLayerDepthSpecies.R
+++ yscripts/R_scripts/VoCCPrioritizr_00d_CostLayerDepthSpecies.R
@@ -4,24 +4,15 @@ library(dplyr)
 library(readr)
 library(stringr)
 
-cost <- readRDS("Cost_Layers/Cost_Raster_Sum.rds")
+
+
 species <- read_rds("Cost_Layers/Cost_RasterStack_bySpecies.rds")
 ns_species <- names(species)
-length(ns_species)
 ns_species2 <- stringr::str_replace_all(string = ns_species, pattern = "[.]", replacement = " ")
-length(ns_species2)
 
-
-depth <- tibble(Species = ns_species2)
 depth2 <- list()
-
-
-
-for (s in 1:length(depth$Species)){
-  depth2[[s]] <- rfishbase::species(ns_species2[s], fields = c("DepthRangeShallow", "DepthRangeDeep"))
-  # depth$MinDepth[s] <- eco$depthRangeShallow
-  # depth$MaxDepth[s] <- eco$depthRangeDeep
-}
+for (s in 1:length(ns_species2)){
+  depth2[[s]] <- rfishbase::species(ns_species2[s], fields = c("DepthRangeShallow", "DepthRangeDeep"))}
 
 names(depth2) <- ns_species # better to 
 depth3 <- do.call(rbind, depth2)
@@ -29,10 +20,26 @@ depth3 <- depth3 %>%
   mutate(Species = ns_species) %>% 
   select(Species, DepthRangeShallow, DepthRangeDeep)
 
-plot(species$Acanthistius.brasilianus)
-
-cost_all <- stackApply(species, indices = nlayers(species), fun = sum)
-plot(cost_all)
-
-writeRaster(cost_all, "Cost_Layers/Cost_Raster_Sum.tif")
+write.csv(depth3, "Cost_Layers/Cost_bySpecies_all.csv")
 depth4 <- na.omit(depth3)
+write.csv(depth4, "Cost_Layers/Cost_bySpecies_NOTall.csv")
+
+
+ep <- depth4 %>% 
+  filter(DepthRangeDeep >= 0 & DepthRangeDeep < 200)
+nrow(ep) #219 species
+mp <- depth4 %>% 
+  filter(DepthRangeDeep >= 200 & DepthRangeDeep < 1000)
+nrow(mp) #277 species
+bap <- depth4 %>% 
+  filter(DepthRangeDeep >= 1000)
+nrow(bap)#115
+
+species_ep <- subset(species, ep$Species)
+species_ep_rs <- stackApply(species_ep, nlayers(species_ep), fun = sum)
+
+plot(log10(species_ep_rs))
+range(species_ep_rs[])
+
+mpas <- st_read("shapefiles_rasters/mpas_v2018/mpas_v2018.shp")
+plot(st_geometry(mpas))
