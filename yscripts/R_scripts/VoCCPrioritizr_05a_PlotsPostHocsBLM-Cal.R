8a594462386848bf13ccab632792bc9fff3f5543
Isaac Brito <ibritomorales@gmail.com>
2020-07-20 10:07:02 +1000

    creating simple function

--- yscripts/R_scripts/VoCCPrioritizr_05a_PlotsPostHocsBLM-Cal.R
+++ yscripts/R_scripts/VoCCPrioritizr_05a_PlotsPostHocsBLM-Cal.R
@@ -5,27 +5,41 @@ library(patchwork)
 library(inflection)
 
 
-rs_final <- read.csv("prioritization_zblm-cal/PostHoc_Calibration_01.csv", stringsAsFactors = FALSE) %>% 
-  # dplyr::mutate(scenario = as.factor(scenario)) %>% 
-  dplyr::mutate(solution = as.factor(solution)) %>% 
-  dplyr::mutate(BLM = as.factor(BLM))
-head(rs_final)
-
-scenarios <- unique(rs_final$scenario)
-blm_list <- vector("list", length = length(scenarios))
-for(i in 1:length(scenarios)) {
-  # Reading the file
-    single <- rs_final %>% 
-      dplyr::filter(scenario == scenarios[i])
-  # Getting the numetaros index for the Sweet spot
-    cost_x <- mean(single$total_cost[single$BLM == 0], na.rm = TRUE)
-    cost_y <- mean(single$total_cost[single$BLM == 1], na.rm = TRUE)
-  # Getting the denominator index for the Sweet spot
-    bound_x <- mean(single$perimeter_m[single$BLM == 0], na.rm = TRUE)
-    bound_y <- mean(single$perimeter_m[single$BLM == 1], na.rm = TRUE)
-  # 
-    blm_list[[i]] <- abs((cost_x - cost_y)/(bound_x - bound_y))
+blm_sweet <- function(posthoc_csv, outdir) { 
+  
+  library(dplyr)
+  # Reading
+    rs_final <- read.csv(posthoc_csv, stringsAsFactors = FALSE) %>% 
+      # dplyr::mutate(scenario = as.factor(scenario)) %>% 
+      dplyr::mutate(solution = as.factor(solution)) %>% 
+      dplyr::mutate(BLM = as.factor(BLM))
+  # Looping
+    scenarios <- unique(rs_final$scenario)
+    blm_list <- vector("list", length = length(scenarios))
+    blm_list <- vector("list", length = length(scenarios))
+    for(i in 1:length(scenarios)) {
+      # Reading the file
+        single <- rs_final %>% 
+          dplyr::filter(scenario == scenarios[i])
+      # Getting the numetaros index for the Sweet spot
+        cost_x <- mean(single$total_cost[single$BLM == 0], na.rm = TRUE)
+        cost_y <- mean(single$total_cost[single$BLM == 1], na.rm = TRUE)
+      # Getting the denominator index for the Sweet spot
+        bound_x <- mean(single$perimeter_m[single$BLM == 0], na.rm = TRUE)
+        bound_y <- mean(single$perimeter_m[single$BLM == 1], na.rm = TRUE)
+      # Calculating the sweet spot
+        blm_list[[i]] <- abs((cost_x - cost_y)/(bound_x - bound_y))
+    }
+    names(blm_list) <- scenarios
+    blm_df <- do.call(rbind, blm_list) %>% 
+      as.data.frame()
+  # Write data frame
+    write.csv(blm_df, paste(outdir, "BLM_sweet", ".csv", sep = ""))
+  
 }
-names(blm_list) <- scenarios
 
+blm_sweet(posthoc_csv = "prioritization_zblm-cal/PostHoc_Calibration_01.csv", 
+          outdir = "prioritization_zblm-cal/")
 
+blm_df <- read.csv("prioritization_zblm-cal/BLM_sweet.csv")
+blm_df[4,2]
