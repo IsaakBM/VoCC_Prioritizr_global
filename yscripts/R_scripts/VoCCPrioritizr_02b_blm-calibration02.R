5a0a0db02f0440fc5864f1391ec75da1ea885230
Isaac Brito <ibritomorales@gmail.com>
2020-09-03 13:01:12 +1000

    adding new file for posthoc

---
+++ yscripts/R_scripts/VoCCPrioritizr_02b_blm-calibration02.R
@@ -0,0 +1,102 @@
+# This code was written by Isaac Brito-Morales (i.britomorales@uq.edu.au)
+# Please do not distribute this code without permission.
+# NO GUARANTEES THAT CODE IS CORRECT
+# Caveat Emptor!
+
+# AIM: 
+# path: 
+
+
+posthoc_marxan <- function(path, outdir, proj.geo, spot) { 
+  
+  library(raster)
+  library(sf)
+  library(dplyr)
+  library(lwgeom)
+  library(doParallel)
+  library(parallel)
+  library(foreach)
+  library(stringr)
+  library(magrittr)
+  
+  # Define all the directories 
+    dir.scenarios <- paste(list.dirs(path = path, full.names = TRUE, recursive = FALSE), sep = "/")
+  # Loop through every scenario-solutionBLM
+    # To allocate iterations dataframes
+      scenario_list <- list()
+      for(i in 1:length(dir.scenarios)) {
+        # Files location
+          out_files <- list.files(path = dir.scenarios[i], pattern = "*cost.*.csv$", full.names = TRUE)
+          pu_shpfile <- list.files(path = dir.scenarios[i], pattern = "*.shp$", full.names = TRUE)
+        # Read shapefile just one time
+          dt_shp <- st_read(pu_shpfile) %>% 
+            st_transform(crs = CRS(proj.geo))
+          var.names <- colnames(dt_shp)
+          dt_shp <- dt_shp %>% 
+            magrittr::set_colnames(ifelse(str_detect(var.names, "(?i).*id*"), "id", 
+                                          ifelse(str_detect(var.names, "(?i)cost"), "cost", var.names)))
+        # Begin the parallel structure
+          UseCores <- 10 #detectCores() -1
+          cl <- makeCluster(UseCores)  
+          registerDoParallel(cl)
+          ls_geom <- list() # empty list to allocate results
+          # A parallel structure to search the different solutions per iterations and calculates boundaries and perimeters 
+            geom_list <- foreach(j = 1:length(out_files), .packages = c("sf", "raster", "dplyr", "lwgeom", "stringr", "magrittr")) %dopar% {
+              # Read outfiles
+                dt <- read.csv(out_files[j], sep = ",", header = TRUE)
+                # Solution's names
+                  name <- basename(out_files[j])
+                  name <- sub(pattern = "*.csv", "", name)
+                    ns <- unlist(strsplit(x = name, split = "_"))
+                    ns_scenario <- paste(ns[1:(length(ns)-2)], collapse = "_")
+                    ns_blm <- unlist(strsplit(x = name, split = "_"))[length(ns)-1]
+                # Split dataframe in solutions and ids/cost
+                  dt_idcost <- dt[,1:2]
+                  dt_solutions <- dt %>% 
+                    dplyr::select(matches("solution"))
+                # Extract from the shapefile the object dt
+                  solutions_list <- list()
+                  for(k in 1:ncol(dt_solutions)) {
+                    single <- cbind(dt_idcost, dt_solutions[k])
+                    dt1 <- single %>% 
+                      filter(single[,3] == "1")
+                    dt2 <- dt_shp[dt_shp$id %in% dt1$id,]
+                    dt3 <- dt2 %>% 
+                      dplyr::summarise(total_cost = sum(cost, na.rm = TRUE, do_union = TRUE))
+                    dt_final <- dt3 %>% mutate(area = st_area(dt3), 
+                                               perimeter = st_perimeter(dt3), 
+                                               solution = colnames(dt1)[3],
+                                               scenario = ns_scenario,
+                                               BLM = ns_blm)
+                    solutions_list[[k]] <- dt_final
+                  }
+                solutions_final <- do.call(rbind, solutions_list)  
+                ls_geom[[j]] <- solutions_final # to allocate results
+                } 
+            stopCluster(cl)
+        
+    # Combine to a dataframe per iteration-scenario
+      geom_list <- do.call(rbind, geom_list)
+      # Cleane the object
+        geom_list <- geom_list %>% 
+          data.frame %>% 
+          dplyr::select(-geometry) %>% 
+          dplyr::mutate(area = as.numeric(area), perimeter = as.numeric(perimeter)) %>% 
+          dplyr::rename(area_m2 = area, perimeter_m = perimeter)
+    # Final list object
+      scenario_list[[i]] <- geom_list
+      }
+  # Final dataframe for all iterations-scenarios in directory path
+    iteration_df <- do.call(rbind, scenario_list)
+    iteration_df <- iteration_df %>% 
+      dplyr::mutate(trade_off = ifelse(BLM == 0, "X", ifelse(BLM == 1, "Y", spot)))
+    name.df <- paste("PostHoc_Calibration")
+    write.csv(iteration_df, paste(outdir, name.df, ".csv", sep = ""), row.names = FALSE)  
+  
+}
+
+  system.time(posthoc_marxan(path = "/QRISdata/Q1216/BritoMorales/Project04b/vfinal-sol_figs/ublm-cal_rce-vocc050_targets-mix",
+                             outdir = "/QRISdata/Q1216/BritoMorales/Project04b/vfinal-sol_figs/ublm-cal_rce-vocc050_targets-mix/",
+                             proj.geo = "+proj=moll +lon_0=0 +datum=WGS84 +units=m +no_defs",
+			     spot = ""))
+
