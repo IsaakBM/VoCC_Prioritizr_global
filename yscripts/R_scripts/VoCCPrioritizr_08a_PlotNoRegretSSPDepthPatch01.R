fb0ce59536359aa838c5d110ae00fe72709a1a6b
Isaac Brito <ibritomorales@gmail.com>
2020-11-10 10:28:01 +1000

    no regret zones across SSPs and Depths

---
+++ yscripts/R_scripts/VoCCPrioritizr_08a_PlotNoRegretSSPDepthPatch01.R
@@ -0,0 +1,235 @@
+# This code was written by Isaac Brito-Morales (i.britomorales@uq.edu.au)
+# Please do not distribute this code without permission.
+# NO GUARANTEES THAT CODE IS CORRECT
+# Caveat Emptor!
+
+no_regret_plots <- function(path, outdir, shp) {
+  
+  library(sf)
+  library(raster)
+  library(dplyr)
+  library(ggplot2)
+  library(stringr)
+  library(patchwork)
+  library(rnaturalearth)
+  library(rnaturalearthdata)
+  library(RColorBrewer)
+  library(foreach)
+  library(doParallel)  
+  
+  # Which directories for climate prioritization scenarios?
+    dir.layers <- paste(list.dirs(path = path, full.names = TRUE, recursive = FALSE), sep = "/")
+    dir.shp <- paste(list.dirs(path = shp, full.names = TRUE, recursive = FALSE), sep = "/")
+    # 
+      ep_dir <- dir.layers[str_detect(string = basename(dir.layers), pattern = "Epi") == TRUE]
+      mp_dir <- dir.layers[str_detect(string = basename(dir.layers), pattern = "Meso") == TRUE]
+      bap_dir <- dir.layers[str_detect(string = basename(dir.layers), pattern = "Bathy") == TRUE]
+      shps <- list.files(path = dir.shp, pattern = "*.shp$", full.names = TRUE)
+      olayers_list <- list(ep_dir, mp_dir, bap_dir)
+    # 
+      y_axis <- c("Epipelagic", "Mesopelagic", "Bathyabyssopelagic")
+      
+  # Loop for every directory
+    # Begin the parallel structure
+      UseCores <- 5
+      cl <- makeCluster(UseCores)  
+      registerDoParallel(cl)
+      gglits <- vector("list", length = length(olayers_list))
+      plots_list <- foreach(i = 1:length(olayers_list), .packages = c("sf", "raster", "dplyr", "ggplot2", "rnaturalearth", "rnaturalearthdata", "RColorBrewer")) %dopar% {
+        
+        # 
+          files_solution <- list.files(path = olayers_list[[i]], pattern = "*Layer_.*.csv$", full.names = TRUE)
+          provinces_csv <- read.csv(list.files(path = olayers_list[[i]], pattern = "*pus-.*.csv$", full.names = TRUE)[1]) %>% 
+            dplyr::arrange(layer) # the same for every ocean layer so that's why [1]
+          pu_shpfile <- st_read(list.files(path = olayers_list[[i]], pattern = ".shp", full.names = TRUE)[1]) # # the same for every ocean layer so that's why [1]
+        
+        # 
+          solutions_csv <- lapply(files_solution, function(x) {
+            single <- read.csv(x)
+            sol_csv <- single %>% 
+              dplyr::mutate(freq_sel = single[, 6]) %>% 
+              dplyr::select(id, cost, freq_sel) %>% 
+              dplyr::arrange(id)})
+        # 
+          no_regret <- solutions_csv[[1]][,3]*solutions_csv[[2]][,3]*solutions_csv[[3]][,3]*solutions_csv[[4]][,3]
+          no_regret_csv <- solutions_csv[[4]] %>% # [[4]] is the "base" dataframe solution but does not matter in this case
+            dplyr::mutate(no_regret = no_regret) %>% 
+            dplyr::select(id, no_regret) %>% 
+            dplyr::arrange(id)
+        
+        # Get the solutions from the corresponding planning unit shapefile (not sure if this is useful but worth to check later)
+          best_freq_sol <- pu_shpfile[pu_shpfile$id %in% no_regret_csv$id, ] %>% 
+            mutate(no_regret = no_regret_csv$no_regret)
+        # Creating the PROVINCES shapefile based on ocean layer
+          provinces_shp <- pu_shpfile %>%
+            dplyr::mutate(provinces = provinces_csv$province) %>% 
+            base::transform(id = as.numeric(factor(provinces))) %>% 
+            dplyr::group_by(id) %>% 
+            dplyr::summarise(prov = sum(id, do_union = TRUE))
+        
+        # Define themes to plot 
+          # Defining themes
+            theme_opts3 <- list(theme(panel.grid.minor = element_blank(),
+                                      panel.grid.major = element_blank(),
+                                      panel.background = element_blank(),
+                                      plot.background = element_rect(fill = "white"),
+                                      panel.border = element_blank(),
+                                      axis.line = element_blank(),
+                                      axis.text.x = element_blank(),
+                                      axis.text.y = element_blank(),
+                                      axis.ticks = element_blank(),
+                                      axis.ticks.length = unit(.25, "cm"), 
+                                      axis.title.x = element_blank(),
+                                      axis.title.y = element_text(face = "plain", size = 25, angle = 90),
+                                      plot.title = element_text(face = "plain", size = 25, hjust = 0.5),
+                                      legend.title = element_text(colour = "black", face = "bold", size = 15),
+                                      legend.text = element_text(colour = "black", face = "bold", size = 10), 
+                                      legend.key.height = unit(2, "cm"),
+                                      legend.key.width = unit(0.9, "cm"),
+                                      plot.tag = element_text(size = 25, face = "bold")))
+          # Color Palette, World borders and Legend
+            pal <- c("#deebf7", "#31a354")
+            world_sf <- ne_countries(scale = "medium", returnclass = "sf") 
+            ranges <- c("0", "1")
+          # Plot
+            gglits[[i]] <- ggplot() + 
+              geom_sf(data = best_freq_sol, aes(group = as.factor(no_regret), fill = as.factor(no_regret)), color = NA) +
+              geom_sf(data = provinces_shp, fill = NA) +
+              geom_sf(data = world_sf, size = 0.05, fill = "grey20") +
+              scale_fill_manual(values = pal,
+                                name = "Selection",
+                                labels = ranges) +
+              # ggtitle(main_tittles[i]) +
+              labs(y = y_axis[i]) +
+              theme_opts3
+        
+      }
+      stopCluster(cl)
+      
+  # No regret across depth
+    UseCores <- 5
+    cl <- makeCluster(UseCores)  
+    registerDoParallel(cl)
+    sflits <- vector("list", length = length(olayers_list))
+    sf_list <- foreach(j = 1:length(olayers_list), .packages = c("sf", "raster", "dplyr", "ggplot2", "rnaturalearth", "rnaturalearthdata", "RColorBrewer")) %dopar% {
+      # 
+        files_solution <- list.files(path = olayers_list[[j]], pattern = "*Layer_.*.csv$", full.names = TRUE)
+        provinces_csv <- read.csv(list.files(path = olayers_list[[j]], pattern = "*pus-.*.csv$", full.names = TRUE)[1]) %>% 
+          dplyr::arrange(layer) # the same for every ocean layer so that's why [1]
+        pu_shpfile <- st_read(list.files(path = olayers_list[[j]], pattern = ".shp", full.names = TRUE)[1]) # # the same for every ocean layer so that's why [1]
+      # 
+        solutions_csv <- lapply(files_solution, function(x) {
+          single <- read.csv(x)
+          sol_csv <- single %>% 
+            dplyr::mutate(freq_sel = single[, 6]) %>% 
+            dplyr::select(id, cost, freq_sel) %>% 
+            dplyr::arrange(id)})
+      # 
+        no_regret <- solutions_csv[[1]][,3]*solutions_csv[[2]][,3]*solutions_csv[[3]][,3]
+        no_regret_csv <- solutions_csv[[4]] %>% # [[4]] is the "base" dataframe solution but does not matter in this case
+          dplyr::mutate(no_regret = no_regret) %>% 
+          dplyr::select(id, no_regret) %>% 
+          dplyr::arrange(id)
+        
+      # Get the solutions from the corresponding planning unit shapefile (not sure if this is useful but worth to check later)
+        best_freq_sol <- pu_shpfile[pu_shpfile$id %in% no_regret_csv$id, ] %>% 
+          mutate(no_regret = no_regret_csv$no_regret)
+        sflits[[j]] <- best_freq_sol
+      }
+      stopCluster(cl)
+  # Getting no regrets from original shapefiles
+    single_shp <- lapply(shps, function(x) {final <- st_read(x)})
+    first <- sf_list[[1]]$no_regret[match(single_shp[[1]]$layer, sf_list[[1]]$id)]
+      first <- ifelse(is.na(first), 0, first) # some NAs due depth
+    second <- sf_list[[2]]$no_regret[match(single_shp[[2]]$layer, sf_list[[2]]$id)]
+      second <- ifelse(is.na(second), 0, second) # some NAs due depth
+    third <- sf_list[[3]]$no_regret[match(single_shp[[3]]$layer, sf_list[[3]]$id)]
+      third <- ifelse(is.na(third), 0, third) # some NAs due depth
+    # Creating the file to plot
+      sf_list[[1]]$no_regret_all <- first*second*third
+      # Define themes to plot 
+        # Defining themes
+          theme_opts3 <- list(theme(panel.grid.minor = element_blank(),
+                                    panel.grid.major = element_blank(),
+                                    panel.background = element_blank(),
+                                    plot.background = element_rect(fill = "white"),
+                                    panel.border = element_blank(),
+                                    axis.line = element_blank(),
+                                    axis.text.x = element_blank(),
+                                    axis.text.y = element_blank(),
+                                    axis.ticks = element_blank(),
+                                    axis.ticks.length = unit(.25, "cm"), 
+                                    axis.title.x = element_blank(),
+                                    axis.title.y = element_text(face = "plain", size = 25, angle = 90),
+                                    plot.title = element_text(face = "plain", size = 25, hjust = 0.5),
+                                    legend.title = element_text(colour = "black", face = "bold", size = 15),
+                                    legend.text = element_text(colour = "black", face = "bold", size = 10), 
+                                    legend.key.height = unit(2, "cm"),
+                                    legend.key.width = unit(0.9, "cm"),
+                                    plot.tag = element_text(size = 25, face = "bold")))
+        # Color Palette, World borders and Legend
+          pal <- c("#deebf7", "#31a354")
+          world_sf <- ne_countries(scale = "medium", returnclass = "sf") 
+          ranges <- c("0", "1")
+        # Plot
+          no_regret_all <- ggplot() + 
+            geom_sf(data = sf_list[[1]], aes(group = as.factor(no_regret_all), fill = as.factor(no_regret_all)), color = NA) +
+            geom_sf(data = world_sf, size = 0.05, fill = "grey20") +
+            scale_fill_manual(values = pal,
+                              name = "Selection",
+                              labels = ranges) +
+            # ggtitle(main_tittles[i]) +
+            # labs(y = y_axis[i]) +
+            theme_opts3
+
+  # Plotting the FINAL FIGURES
+    # Defining themes
+      theme_opts3 <- list(theme(panel.grid.minor = element_blank(),
+                                panel.grid.major = element_blank(),
+                                panel.background = element_blank(),
+                                plot.background = element_rect(fill = "white"),
+                                panel.border = element_blank(),
+                                axis.line = element_blank(),
+                                axis.text.x = element_blank(),
+                                axis.text.y = element_blank(),
+                                axis.ticks = element_blank(),
+                                axis.ticks.length = unit(.25, "cm"),
+                                axis.title.x = element_blank(),
+                                axis.title.y = element_text(face = "plain", size = 25, angle = 90),
+                                plot.title = element_text(face = "plain", size = 25, hjust = 0.5),
+                                legend.title = element_text(colour = "black", face = "bold", size = 15),
+                                legend.text = element_text(colour = "black", face = "bold", size = 10),
+                                legend.key.height = unit(2, "cm"),
+                                legend.key.width = unit(0.9, "cm"),
+                                plot.tag = element_text(size = 25, face = "bold")))
+      # CALIBRATION PLOTS
+      p3 <-   ((plots_list[[1]] / plots_list[[2]] / plots_list[[3]]) | no_regret_all) +
+        plot_layout(guides = "collect") +
+        plot_annotation(tag_prefix = "",
+                        tag_levels = "A",
+                        tag_suffix = ".",) +
+        theme_opts3 +
+        ggsave(paste(outdir, paste("no-regrets-all", ".pdf", sep = ""), sep = ""), width = 30, height = 20, dpi = 300)
+}
+
+system.time(no_regret_plots(path = "/QRISdata/Q1216/BritoMorales/Project04b/vfinal-sol_figs_02/ublm-cal_1020rce-vocc1050_targets-mix_rawcost_noduplicates",
+                           outdir = "/QRISdata/Q1216/BritoMorales/Project04b/vfinal-sol_figs_02/ublm-cal_1020rce-vocc1050_targets-mix_rawcost_noduplicates/",
+                           shp = "/QRISdata/Q1216/BritoMorales/Project04b/shapefiles_rasters/01_abnjs_nofilterdepth"))
+
+
+###############################
+
+
+# TABLE with the area of protection of the no regret area, cost and vocc comparing with the different climate scenarios 
+# epi total area of protection; total opportunity cost; vocc [vocc126-vocc245-vocc585] ; RCE index [rce126-rce245-rce585]
+# meso
+# bathy
+
+# nrow(no_regret_csv) # 90065
+# nrow(no_regret_csv[no_regret_csv$no_regret == 1,]) # 7899
+# 
+# (7899*100)/90065
+# 
+# nrow(solutions_csv[[1]])
+# nrow(no_regret_csv[solutions_csv[[1]]$freq_sel == 1,]) # 21627
+# (21627*100)/90065
