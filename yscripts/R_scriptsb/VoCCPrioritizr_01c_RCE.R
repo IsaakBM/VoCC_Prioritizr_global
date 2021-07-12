aba00f9b744d501f45def1bf70267939c6f8ffa2
Isaac Brito-Morales <i.britomorales@uq.edu.au>
2021-07-12 16:51:10 +1000

    adding new tidy scripts

---
+++ yscripts/R_scriptsb/VoCCPrioritizr_01c_RCE.R
@@ -0,0 +1,64 @@
+
+
+rce <- function(omon, slp, from, to, outdir, ...) {
+  
+  library(raster)
+  library(dplyr)
+  library(sf)
+  library(stringr)
+  
+  # A function to flip the data
+    flip_rs <- function(data) {
+      for (i in 1:nlayers(data)) {
+        if (i == 1) {
+          single <- subset(data, i)
+          rs <- as.data.frame(rasterToPoints(single))
+          rs[,1] <- ifelse(rs[,1] < 0, rs[,1] + 180, rs[,1] - 180)
+          rs2 <- rasterFromXYZ(rs)
+          st <- rs2
+        } else {
+          single <- subset(data, i)
+          rs <- as.data.frame(rasterToPoints(single))
+          rs[,1] <- ifelse(rs[,1] < 0, rs[,1] + 180, rs[,1] - 180)
+          rs2 <- rasterFromXYZ(rs)
+          st <- stack(st, rs2)
+        }
+      }
+      return(st)
+    }
+  
+  # Getting the years/month to calculate de RCE index
+    names.yrs2 <- paste("X", seq(as.Date(paste(from, "1", "1", sep = "/")), as.Date(paste(to, "12", "1", sep = "/")), by = "month"), sep = "") %>%
+      str_replace_all(pattern = "-", replacement = ".")
+  # Read, subset and flip the data
+    rs1 <- readAll(stack(omon)) %>% 
+      subset(names.yrs2) %>% 
+      flip_rs()
+  # Get ANNUAL min and max to estimate the rage to get the RCE
+    index1 <- rep(1:nlayers(rs1), each = 12, length.out = nlayers(rs1))
+    rs1_min <- stackApply(x = rs1, indices = index1, fun = min)
+    rs1_max <- stackApply(x = rs1, indices = index1, fun = max)
+    # Range among the period selected
+      rs1_range <- rs1_max - rs1_min
+      rs1_range_mean <- stackApply(x = rs1_range, indices = nlayers(rs1_range), fun = mean) # calculate the annual mean range for the period selected
+  # Get the slope
+    slp  <- (readAll(raster(slp))*10)*8
+  
+  # Calculate the RCE
+    RCE <- abs(slp/rs1_range_mean) # absolute value since it has no unit
+    # Write the object
+      ns <- basename(omon)
+        olayer <- unlist(strsplit(x = ns, split = "_"))[1]
+        model <- unlist(strsplit(x = ns, split = "_"))[3]
+        ssp <- unlist(strsplit(x = ns, split = "_"))[4]
+        name.rs <- paste(olayer, "RCE", model, ssp, paste(from, to, sep = "-"), sep = "_")
+      # Write the raster 
+        writeRaster(RCE, paste(outdir, name.rs, ".tif", sep = ""), overwrite = TRUE)
+}
+
+system.time(rce(omon = "/QRISdata/Q1216/BritoMorales/Project04b/CMIP6_zrasters_zensemble/thetao_05deg/ssp126/01_SurfaceLayer/01-sf_thetao_AEMean_ssp126_r1i1p1f1_2015-2100.grd", 
+                slp = "/QRISdata/Q1216/BritoMorales/Project04b/vocc_a_nointer/slpTrends/ssp126/01_SurfaceLayer/slpTrends_01-sf_AEMean_ssp126_r1i1p1f1_2020-2100_.tif", 
+                from = 2015, 
+                to = 2020, 
+                outdir = "/QRISdata/Q1216/BritoMorales/Project04b/vocc_a_nointer/RCE/"))
+
--- yscripts/R_scriptsb/VoCCPrioritizr_01c_VoCCModel.R
+++ yscripts/R_scriptsb/VoCCPrioritizr_01c_VoCCModel.R
@@ -77,7 +77,7 @@
           # Calculate VoCC for every depth
             list.vocc <- vector("list", length(rs_multi)) # to allocate results
             # Begin the parallel structure
-              UseCores <- 3 # Define how many cores
+              UseCores <- 2 # Define how many cores
               cl <- makeCluster(UseCores)  
               registerDoParallel(cl) # Register CoreCluster
               # Parallel Loop
@@ -92,7 +92,7 @@
                       c <- gVoCC(a,b)
                       c$voccMag[] <- ifelse(is.infinite(c$voccMag[]), NA, c$voccMag[]) # replace inf with NAs (just in case)
                     # Writing VoCC rasters by depth
-                      ns <- basename(files_rs[k])
+                      ns <- basename(files_rs[j])
                         var1 <- paste("voccMag", unlist(strsplit(ns, "_"))[1], sep = "_")
                         var2 <- paste("slpTrends", unlist(strsplit(ns, "_"))[1], sep = "_")
                         var3 <- paste("seTrends", unlist(strsplit(ns, "_"))[1], sep = "_")
@@ -107,40 +107,40 @@
                       writeRaster(a[[1]], paste(outdir, name.rs2, "_.tif", sep = ""), overwrite = TRUE)
                       writeRaster(a[[2]], paste(outdir, name.rs3, "_.tif", sep = ""), overwrite = TRUE)
                     
-                  # Calculate Trajectories for every depth
-                    # The mean of the whole period
-                      mn <- mean(rs, na.rm = T)
-                    # get the set of starting cells for the trajectories
-                      lonlat <- na.omit(data.frame(xyFromCell(c[[1]], 1:ncell(c[[1]])), c[[1]][], c[[2]][], mn[]))[,1:2]
-                    # Calculate trajectories.
-                      traj <- voccTraj(lonlat, c[[1]], c[[2]], mn = mn, tyr = nlayers(rs), correct = TRUE)
-                      clas <- trajClas(traj, c[[1]], c[[2]], mn = mn, trajSt = 16, tyr =  nlayers(rs), nmL = 20, smL = 100, Nend = 45, Nst = 15, NFT = 70)
-                      # my_col <- c('gainsboro','darkseagreen1','coral4','firebrick2','mediumblue','darkorange1','magenta1','cadetblue1','yellow1')
-                    # Isolate trajectory classes for individual rasters
-                      r <- ratify(clas[[7]]) # trajectory class from the stack element
-                      rat_r <-levels(r)[[1]] # numbers for classes
-                      rat_r$trajcat <- c("non-moving", 
-                                         "slow-moving", 
-                                         "internal-sinks", 
-                                         "boundary-sinks", 
-                                         "sources", 
-                                         "relative-sinks", 
-                                         "corridors",
-                                         "divergence",
-                                         "convergence")[sort(unique(clas[[7]][]))] # extract the appropiate classes from the raster
-                      levels(r) <- rat_r
-                      # names for loop and for writing raster
-                        traj_features <- levels(r)[[1]][[1]]
-                        traj_names <- levels(r)[[1]][[2]]
-                      # Loop to get individuals rasters
-                        traj_list <- vector("list", length(traj_features))
-                        for(i in 1:length(traj_features)) {
-                          single <- r
-                          single[] <- ifelse(single[] == traj_features[i], single[], NA)
-                          var.traj <- paste(traj_names[i], unlist(strsplit(ns, "_"))[1], sep = "_")
-                          name.traj <- paste(var.traj, model, rcp, ensm, paste(from, to, sep = "-"), sep = "_")
-                          writeRaster(single, paste(outdir, name.traj, "_.tif", sep = ""), overwrite = TRUE)
-                        }
+                  # # Calculate Trajectories for every depth
+                  #   # The mean of the whole period
+                  #     mn <- mean(rs, na.rm = T)
+                  #   # get the set of starting cells for the trajectories
+                  #     lonlat <- na.omit(data.frame(xyFromCell(c[[1]], 1:ncell(c[[1]])), c[[1]][], c[[2]][], mn[]))[,1:2]
+                  #   # Calculate trajectories.
+                  #     traj <- voccTraj(lonlat, c[[1]], c[[2]], mn = mn, tyr = nlayers(rs), correct = TRUE)
+                  #     clas <- trajClas(traj, c[[1]], c[[2]], mn = mn, trajSt = 16, tyr =  nlayers(rs), nmL = 20, smL = 100, Nend = 45, Nst = 15, NFT = 70)
+                  #     # my_col <- c('gainsboro','darkseagreen1','coral4','firebrick2','mediumblue','darkorange1','magenta1','cadetblue1','yellow1')
+                  #   # Isolate trajectory classes for individual rasters
+                  #     r <- ratify(clas[[7]]) # trajectory class from the stack element
+                  #     rat_r <-levels(r)[[1]] # numbers for classes
+                  #     rat_r$trajcat <- c("non-moving", 
+                  #                        "slow-moving", 
+                  #                        "internal-sinks", 
+                  #                        "boundary-sinks", 
+                  #                        "sources", 
+                  #                        "relative-sinks", 
+                  #                        "corridors",
+                  #                        "divergence",
+                  #                        "convergence")[sort(unique(clas[[7]][]))] # extract the appropiate classes from the raster
+                  #     levels(r) <- rat_r
+                  #     # names for loop and for writing raster
+                  #       traj_features <- levels(r)[[1]][[1]]
+                  #       traj_names <- levels(r)[[1]][[2]]
+                  #     # Loop to get individuals rasters
+                  #       traj_list <- vector("list", length(traj_features))
+                  #       for(i in 1:length(traj_features)) {
+                  #         single <- r
+                  #         single[] <- ifelse(single[] == traj_features[i], single[], NA)
+                  #         var.traj <- paste(traj_names[i], unlist(strsplit(ns, "_"))[1], sep = "_")
+                  #         name.traj <- paste(var.traj, model, rcp, ensm, paste(from, to, sep = "-"), sep = "_")
+                  #         writeRaster(single, paste(outdir, name.traj, "_.tif", sep = ""), overwrite = TRUE)
+                  #       }
                 }
                 stopCluster(cl)
         }
@@ -156,8 +156,8 @@
       #                        from = "2050",
       #                        to = "2100"))
       
-      system.time(vocc_model(path = "Inputs/CMIP6_zrasters_zensemble/ssp126",
-                             outdir = "Inputs/CMIP6_zrasters_zensemble/ssp126/",
+      system.time(vocc_model(path = "Inputs/CMIP6_zrasters_zensemble/ssp585",
+                             outdir = "Inputs/ClimateChange/SlpTrend/",
                              variable = "tob",
                              region = "Inputs/ETOPO1_05deg/ETOPO1_ocean.grd",
                              flip = TRUE,
