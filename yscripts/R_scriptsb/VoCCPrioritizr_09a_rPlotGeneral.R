9161813668ca582278a2edb64caa6e0091effa9e
Isaac Brito-Morales <i.britomorales@uq.edu.au>
2021-07-28 10:23:33 +1000

    tidy run script general code

--- yscripts/R_scriptsb/VoCCPrioritizr_09a_rPlotGeneral.R
+++ yscripts/R_scriptsb/VoCCPrioritizr_09a_rPlotGeneral.R
@@ -1,42 +1,59 @@
-
-
-
+# This code was written by Isaac Brito-Morales (i.britomorales@uq.edu.au)
+# Please do not distribute this code without permission.
+# NO GUARANTEES THAT CODE IS CORRECT
+# Caveat Emptor!
 
 source("yscripts/R_scriptsb/VoCCPrioritizr_08a_fPlotGeneral.R")
 
+####################################################################################
+####### 0.- BritoMorales_ED_Fi_1
+####################################################################################
+# Richness
+  ep_rich <- plot_rich(data = "Output/FeaturesOLayer/epipelagic.csv", sfdom = pld_ep, sfprov = lg) + theme(legend.position = "none")
+  mp_rich <- plot_rich(data = "Output/FeaturesOLayer/mesopelagic.csv", sfdom = pld_mp, sfprov = glw)
+  bap_rich <- plot_rich(data = "Output/FeaturesOLayer/BathyAbyssopelagic.csv", sfdom = pld_bap, sfprov = glw) + theme(legend.position = "none")
+  sflr_rich <- plot_rich(data = "Output/FeaturesOLayer/seafloor.csv", sfdom = pld_sflr, sfprov = sflr) + theme(legend.position = "none")
+# Cost
+  ep_cost <- plot_cost(data = "Output/CostOLayer/02-epipelagic_CostSFTotal.rds", sfprov = lg) + theme(legend.position = "none")
+  mp_cost <- plot_cost(data = "Output/CostOLayer/03-mesopelagic_CostSFTotal.rds", sfprov = glw)
+  bap_cost <- plot_cost(data = "Output/CostOLayer/04-bathyabyssopelagic_CostSFTotal.rds", sfprov = glw) + theme(legend.position = "none")
+  sflr_cost <- plot_cost(data = "Output/CostOLayer/05-seafloorCostSFTotal.rds", sfprov = sflr) + theme(legend.position = "none")
+# 
+  pED01 <- patchwork::wrap_plots(ep_cost, mp_cost, bap_cost, sflr_cost, 
+                                 ep_rich, mp_rich, bap_rich, sflr_rich, ncol = 2, byrow = FALSE) +
+    plot_annotation(tag_prefix = "(",
+                    tag_levels = "a", 
+                    tag_suffix = ")",)
+  ggsave("Figures/MS_v1/BritoMorales_ED_Fi_1.pdf", plot = pED01, width = 30, height = 25, dpi = 300, limitsize = FALSE)
+  ggsave("Figures/MS_v1/BritoMorales_ED_Fi_1.png", plot = pED01, width = 30, height = 25, dpi = 300, limitsize = FALSE)
 
-p1 <- plot_VoCC(path = "Inputs/General", world_sf, pldom)
-p2 <- plot_RCE(path = "Inputs/General", world_sf, pldom)
+####################################################################################
+####### 0.- BritoMorales_ED_Fi_2
+####################################################################################
+# VoCC
+  p1 <- plot_VoCC(path = "Inputs/General", world_sf, pldom)
+  p1.1 <- patchwork::wrap_plots(p1, ncol = 3, byrow = TRUE) +
+    plot_layout(guides = "collect") +
+    plot_annotation(tag_prefix = "(",
+                    tag_levels = "a", 
+                    tag_suffix = ")",)
+  ggsave("Figures/MS_v1/BritoMorales_ED_Fi_2.pdf", plot = p1.1, width = 35, height = 25, dpi = 300, limitsize = FALSE)
+  ggsave("Figures/MS_v1/BritoMorales_ED_Fi_2.png", plot = p1.1, width = 35, height = 25, dpi = 300, limitsize = FALSE)
 
-p1.1 <- patchwork::wrap_plots(p1, ncol = 3, byrow = TRUE) +
-  plot_layout(guides = "collect") +
-  plot_annotation(tag_prefix = "(",
-                  tag_levels = "a", 
-                  tag_suffix = ")",)
-ggsave("Figures/MS_v1/BritoMorales_ED_Fi_2.pdf", plot = p1.1, width = 35, height = 25, dpi = 300, limitsize = FALSE)
-ggsave("Figures/MS_v1/BritoMorales_ED_Fi_2.png", plot = p1.1, width = 35, height = 25, dpi = 300, limitsize = FALSE)
-
-
-p1.2 <- patchwork::wrap_plots(p2, ncol = 3, byrow = TRUE) +
-  plot_layout(guides = "collect") +
-  plot_annotation(tag_prefix = "(",
-                  tag_levels = "a", 
-                  tag_suffix = ")",)
-ggsave("Figures/MS_v1/BritoMorales_ED_Fi_3.pdf", plot = p1.2, width = 35, height = 25, dpi = 300, limitsize = FALSE)
-ggsave("Figures/MS_v1/BritoMorales_ED_Fi_3.png", plot = p1.2, width = 35, height = 25, dpi = 300, limitsize = FALSE)
+####################################################################################
+####### 0.- BritoMorales_ED_Fi_3
+####################################################################################  
+# RCE
+  p2 <- plot_RCE(path = "Inputs/General", world_sf, pldom)
+  p1.2 <- patchwork::wrap_plots(p2, ncol = 3, byrow = TRUE) +
+    plot_layout(guides = "collect") +
+    plot_annotation(tag_prefix = "(",
+                    tag_levels = "a", 
+                    tag_suffix = ")",)
+  ggsave("Figures/MS_v1/BritoMorales_ED_Fi_3.pdf", plot = p1.2, width = 35, height = 25, dpi = 300, limitsize = FALSE)
+  ggsave("Figures/MS_v1/BritoMorales_ED_Fi_3.png", plot = p1.2, width = 35, height = 25, dpi = 300, limitsize = FALSE)
 
 
 
 
-ep <- plot_rich(data = "Output/FeaturesOLayer/epipelagic.csv", sfdom = pld_ep, sfprov = lg)
-mp <- plot_rich(data = "Output/FeaturesOLayer/mesopelagic.csv", sfdom = pld_mp, sfprov = glw)
-bap <- plot_rich(data = "Output/FeaturesOLayer/BathyAbyssopelagic.csv", sfdom = pld_bap, sfprov = glw)
-sflr <- plot_rich(data = "Output/FeaturesOLayer/seafloor.csv", sfdom = pld_sflr, sfprov = sflr)
-p1.3 <- patchwork::wrap_plots(ep, mp, bap, sflr, ncol = 1, byrow = FALSE) +
-  plot_layout(guides = "collect") +
-  plot_annotation(tag_prefix = "(",
-                  tag_levels = "a", 
-                  tag_suffix = ")",)
-ggsave("Figures/MS_v1/BritoMorales_ED_Fi_1.pdf", plot = p1.3, width = 15, height = 25, dpi = 300, limitsize = FALSE)
-ggsave("Figures/MS_v1/BritoMorales_ED_Fi_1.png", plot = p1.3, width = 15, height = 25, dpi = 300, limitsize = FALSE)
 
