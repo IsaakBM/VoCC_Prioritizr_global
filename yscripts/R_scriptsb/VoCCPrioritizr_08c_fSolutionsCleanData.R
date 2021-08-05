d7b23af2f70f64b938ffa923759f049c26283ae7
Isaac Brito-Morales <i.britomorales@uq.edu.au>
2021-08-06 09:28:31 +1000

    cleanning

--- yscripts/R_scriptsb/VoCCPrioritizr_08c_fSolutionsCleanData.R
+++ yscripts/R_scriptsb/VoCCPrioritizr_08c_fSolutionsCleanData.R
@@ -23,24 +23,6 @@ library(nngeo)
 
 source("yscripts/R_scriptsb/VoCCPrioritizr_Help.R")
 
-data1 <- "Prioritisation/PrioritizrSolutionsCost/features_10100/03_MesopelagicLayer_ssp126_0_1.rds"
-sfdom <- pld_mp1
-sfprov <- lg
-mpas <- mpas_ep
-vmes <- vmes_ep
-
-data2 <- "Prioritisation/PrioritizrSolutionsCost/features_10100/03_MesopelagicLayer_ssp245_0_1.rds"
-sfdom <- pld_mp
-sfprov <- lg
-mpas <- mpas_ep
-vmes <- vmes_ep
-
-data3 <- "Prioritisation/PrioritizrSolutionsCost/features_10100/03_MesopelagicLayer_ssp585_0_1.rds"
-sfdom <- pld_mp
-sfprov <- lg
-mpas <- mpas_ep
-vmes <- vmes_ep
-
 ####################################################################################
 ####### 3.- Data No Regret climatic by Ocean Layer
 ####################################################################################
@@ -86,119 +68,65 @@ vmes <- vmes_ep
 ####################################################################################
 # 
   data_NoReg <- function(data1, data2, data3, data4, sfdom, mpas, vmes) {
+    
     sfdom <- sfdom %>% 
       dplyr::rename(id = pu)
-    dff1 <- paste(data1$solution_f, data2$solution_f, data3$solution_f, data4$solution_f, sep = "_")
-    sel <- unlist(lapply(dff1, function(x){f <- ifelse(sum(unlist(strsplit(x, "_")) == "0") <= 2, x, 0)})) # individual laer solution == 0
     
-    epSF <- sfdom %>% 
-      dplyr::mutate(solutions = dff1) %>% 
-      dplyr::filter(solutions %in%  c("Epi_Meso_Bathy_0", "Epi_Meso_Bathy_Sea")) %>% 
-      as_tibble() %>% 
-      dplyr::select(-geometry)
-    epSF <- left_join(sfdom, epSF, "id") %>% 
-      dplyr::mutate_all(~replace(., is.na(.), 0)) #%>% 
-      # dplyr::filter(!(id %in% mpas$id)) %>% 
-      # dplyr::filter(!(id %in% vmes$id))
-    # round((length(epSF$solutions[epSF$solutions == "Epi_Meso_Bathy_Sea"])/nrow(epSF))*100, 2)
-    # round((length(epSF$solutions[epSF$solutions == "Epi_Meso_Bathy_0"])/nrow(epSF))*100, 2)
+    plg <- sfdom %>% 
+      dplyr::mutate(solutionAll = data1$solution*data2$solution*data3$solution)
+    ver <- sfdom %>% 
+      dplyr::mutate(solutionAll = data1$solution*data2$solution*data3$solution*data4$solution) %>% 
+      dplyr::mutate(solutionAll = ifelse(solutionAll == 1, 3, solutionAll))
     
+    ff <- sfdom %>% 
+      dplyr::mutate(solution = plg$solutionAll+ver$solutionAll) %>% 
+      dplyr::filter(!(id %in% mpas$id)) %>% 
+      dplyr::filter(!(id %in% vmes$id))
+    
+    # Defining generalities
+    brd <- sfdom %>% 
+      summarise(borders = sum(id, do_union = TRUE))
+    pal <- c("#deebf7", "#ec7014", "#0570b0")
+    ranges <- c("Not Selected", "Pelagic (12%)", "Pelagic + Seafloor (6%)")
+    # Defining themes
+    theme_opts3 <- list(theme(panel.grid.minor = element_blank(),
+                              panel.grid.major = element_blank(),
+                              panel.background = element_blank(),
+                              plot.background = element_rect(fill = "white"),
+                              panel.border = element_blank(),
+                              axis.line = element_blank(),
+                              axis.text.x = element_blank(),
+                              axis.text.y = element_blank(),
+                              axis.ticks = element_blank(),
+                              axis.ticks.length = unit(.25, "cm"),
+                              axis.title.x = element_blank(),
+                              axis.title.y = element_text(face = "plain", size = 25, angle = 90),
+                              plot.title = element_text(face = "plain", size = 18, hjust = 0.5),
+                              legend.title = element_text(colour = "black", face = "bold", size = 25),
+                              legend.text = element_text(colour = "black", face = "plain", size = 10),
+                              legend.key.height = unit(0.8, "cm"),
+                              legend.key.width = unit(0.7, "cm"),
+                              plot.tag = element_text(size = 25, face = "bold")))
+    
+    # Create the ggplot
+    gg_list <- ggplot() +
+      geom_sf(data = ff, aes(group = as.factor(solution), fill = as.factor(solution)), color = NA) +
+      geom_sf(data = brd, fill = NA, color = "black", lwd = 0.3) +
+      geom_sf(data = world_sf, size = 0.05, fill = "grey20") +
+      scale_fill_manual(values = pal,
+                        name = "",
+                        labels = ranges) +
+      ggtitle("Low regret areas for climate-smart networks") +
+      labs(y = "") +
+      theme_opts3
+    
+    return(gg_list)
   }
-
-  sfdom <- pld_ep1
-  data1 <- epF
-  data2 <- mpF
-  data3 <- bapF
-  data4 <- sflrF
-  mpas <- mpas_ep
-  vmes <- vmes_ep
-  
-  sfdom <- sfdom %>% 
-    dplyr::rename(id = pu)
-  
-  plg <- sfdom %>% 
-    dplyr::mutate(solutionAll = data1$solution*data2$solution*data3$solution)
-  ver <- sfdom %>% 
-    dplyr::mutate(solutionAll = data1$solution*data2$solution*data3$solution*data4$solution) %>% 
-    dplyr::mutate(solutionAll = ifelse(solutionAll == 1, 3, solutionAll))
-    
-  final <- sfdom %>% 
-    dplyr::mutate(solution = plg$solutionAll+ver$solutionAll) %>% 
-    dplyr::filter(!(id %in% mpas$id)) %>% 
-    dplyr::filter(!(id %in% vmes$id))
   
   
-  round((nrow(testNAll[testNAll$solutionAll == 1,])/90065)*100)  # just the epi
-  round((nrow(testAll[testAll$solutionAll == 1,])/90065)*100) # All
-  
-  
-  
-  # unique(epF$solution_f)
-  # unique(mpF$solution_f)
-  # unique(bapF$solution_f)
-  # unique(sflrF$solution_f)
+  # round((nrow(testNAll[testNAll$solutionAll == 1,])/90065)*100)  # just the epi
+  # round((nrow(testAll[testAll$solutionAll == 1,])/90065)*100) # All
   # 
-  # epSF <- sfdom %>% 
-  #   dplyr::mutate(solutions = sel) %>% 
-  #   dplyr::filter(solutions %in%  c("Epi_Meso_Bathy_0", "Epi_Meso_Bathy_Sea")) %>% 
-  #   as_tibble() %>% 
-  #   dplyr::select(-geometry)
-  # epSF <- left_join(sfdom, epSF, "id") %>% 
-  #   dplyr::mutate_all(~replace(., is.na(.), 0)) %>% 
-  #   dplyr::filter(!(id %in% mpas$id)) %>% 
-  #   dplyr::filter(!(id %in% vmes$id))
-  # 
-  # 
-  # length(epSF$solutions[epSF$solutions == "Epi_Meso_Bathy_0"])
-  # length(epSF$solutions[epSF$solutions == "Epi_Meso_Bathy_Sea"])
-  # 
-  # round((length(epSF$solutions[epSF$solutions == "Epi_Meso_Bathy_Sea"])/nrow(epSF))*100, 1)
-  # round((length(epSF$solutions[epSF$solutions == "Epi_Meso_Bathy_0"])/nrow(epSF))*100, 1)
-  # 
-  # unique(epSF$solutions)
-  # 
-  # a <- epSF %>% 
-  #   dplyr::filter(solutions == "Epi_Meso_Bathy_0")
-  # b <- epSF %>% 
-  #   dplyr::filter(solutions == "Epi_Meso_Bathy_Sea")
-  
-  brd <- sfdom %>% 
-    summarise(borders = sum(id, do_union = TRUE))
-  
-  # Defining generalities
-  pal <- c("#deebf7", "#ec7014", "#0570b0")
-  ranges <- c("Not Selected", "Pelagic (12%)", "Pelagic + Seafloor (6%)")
-  # Defining themes
-  theme_opts3 <- list(theme(panel.grid.minor = element_blank(),
-                            panel.grid.major = element_blank(),
-                            panel.background = element_blank(),
-                            plot.background = element_rect(fill = "white"),
-                            panel.border = element_blank(),
-                            axis.line = element_blank(),
-                            axis.text.x = element_blank(),
-                            axis.text.y = element_blank(),
-                            axis.ticks = element_blank(),
-                            axis.ticks.length = unit(.25, "cm"),
-                            axis.title.x = element_blank(),
-                            axis.title.y = element_text(face = "plain", size = 25, angle = 90),
-                            plot.title = element_text(face = "plain", size = 18, hjust = 0.5),
-                            legend.title = element_text(colour = "black", face = "bold", size = 25),
-                            legend.text = element_text(colour = "black", face = "plain", size = 10),
-                            legend.key.height = unit(0.8, "cm"),
-                            legend.key.width = unit(0.7, "cm"),
-                            plot.tag = element_text(size = 25, face = "bold")))
-  # Create the ggplot
-  gg_list <- ggplot() +
-    geom_sf(data = final, aes(group = as.factor(solution), fill = as.factor(solution)), color = NA) +
-    geom_sf(data = brd, fill = NA, color = "black", lwd = 0.3) +
-    geom_sf(data = world_sf, size = 0.05, fill = "grey20") +
-    scale_fill_manual(values = pal,
-                      name = "",
-                      labels = ranges) +
-    ggtitle("Low regret areas for climate-smart networks") +
-    labs(y = "") +
-    theme_opts3
-  
-  ggsave("Figures/MS_v1/BritoMorales_Fi_3.pdf", plot = gg_list, width = 12, height = 6, dpi = 300, limitsize = FALSE)
-  ggsave("Figures/MS_v1/BritoMorales_Fi_3.png", plot = gg_list, width = 12, height = 6, dpi = 300, limitsize = FALSE)
+  # ggsave("Figures/MS_v1/BritoMorales_Fi_3.pdf", plot = gg_list, width = 12, height = 6, dpi = 300, limitsize = FALSE)
+  # ggsave("Figures/MS_v1/BritoMorales_Fi_3.png", plot = gg_list, width = 12, height = 6, dpi = 300, limitsize = FALSE)
 
